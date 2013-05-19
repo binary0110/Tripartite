95d644f62cf81889aeb1fa8325ef4d04d4ed8abf
Rashmi Patel <rails@binary0110.co.uk>
2013-05-19 16:48:16 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tripartite::Application.config.secret_token = '73974586284f0f1933d4cbe4591a10436ec4dcfad071eca3c2e1bdbe5e27f2e3a60cea21c0a13379c4b49a307244b2cb59fcb911284363f38ea7def2640134c9'
